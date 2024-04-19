3daabed4bd38655cf4e4a0755a121b33164321bc
Jack Brinkman <jack.brinkman@csiro.au>
2024-04-19 14:10:10 +1000

    Constraint service now reads JSON from configurable JSON path

--- lists-service/src/main/java/au/org/ala/listsapi/service/ConstraintService.java
+++ lists-service/src/main/java/au/org/ala/listsapi/service/ConstraintService.java
@@ -1,9 +1,7 @@
 package au.org.ala.listsapi.service;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,49 +10,26 @@ import au.org.ala.listsapi.model.ConstraintListItem;
 import au.org.ala.listsapi.model.ConstraintType;
 import au.org.ala.listsapi.model.InputSpeciesList;
 import au.org.ala.listsapi.model.SpeciesList;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConstraintService {
-  private static final Logger logger = LoggerFactory.getLogger(ConstraintService.class);
 
-  @Qualifier("webApplicationContext")
-  @Autowired private ResourceLoader resourceLoader;
+  @Value("${constraints.file}")
+  private String constraintsFile;
 
   private Map<String, List<ConstraintListItem>> constraints = null;
 
-  private String getJson() {
-    String out = null;
-    Resource resource = resourceLoader.getResource("classpath:/constraints.json");
-
-    // Don't attempt to read the resource content if it does not exist
-    if (!resource.exists()) return null;
-
-    try {
-      // Read the content of the JSON file
-      byte[] bytes = Files.readAllBytes(Paths.get(resource.getURI()));
-      out = new String(bytes, StandardCharsets.UTF_8);
-    } catch (IOException ex) {
-      logger.error(ex.getMessage());
-    }
-
-    return out;
-  }
-
   @PostConstruct
-  private void init() throws JsonProcessingException {
+  private void init() throws IOException {
     ObjectMapper objectMapper = new ObjectMapper();
-    String json = getJson();
+    File json = new File(constraintsFile);
 
     TypeReference<HashMap<String,List<ConstraintListItem>>> typeRef = new TypeReference<>() {};
     constraints = objectMapper.readValue(json, typeRef);
