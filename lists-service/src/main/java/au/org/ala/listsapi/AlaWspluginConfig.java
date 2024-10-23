b5dfc64ceaefbb95dab381e6b8d8a0fed2341562
Jack Brinkman <jack.brinkman@csiro.au>
2024-10-23 13:36:59 +1000

    Added webService & related classed for m2m authenticated requests

---
+++ lists-service/src/main/java/au/org/ala/listsapi/AlaWspluginConfig.java
@@ -0,0 +1,57 @@
+package au.org.ala.listsapi;
+
+import au.org.ala.listsapi.util.auth.TokenClient;
+import au.org.ala.listsapi.util.auth.TokenInterceptor;
+import au.org.ala.listsapi.util.auth.TokenService;
+import org.pac4j.core.context.session.SessionStore;
+import org.pac4j.oidc.config.OidcConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class AlaWsPluginConfig {
+
+    @Value("${webservice.client-id}")
+    String clientId;
+
+    @Value("${webservice.client-secret}")
+    String clientSecret;
+
+    @Value("${webservice.jwt-scopes}")
+    String jwtScopes;
+
+    @Value("${webservices.cache-tokens:true}")
+    boolean cacheTokens;
+
+    @Bean
+    TokenClient tokenClient(
+            @Autowired(required = false) OidcConfiguration oidcConfiguration
+    ) {
+        return new TokenClient(oidcConfiguration);
+    }
+
+    @Bean
+    TokenService tokenService(
+            @Autowired(required = false) OidcConfiguration oidcConfiguration,
+            @Autowired(required = false) SessionStore sessionStore,
+            @Autowired TokenClient tokenClient) {
+        // note not injecting PAC4j Config here due to potential circular dependency
+        return new TokenService(oidcConfiguration,
+                sessionStore, tokenClient, clientId, clientSecret, jwtScopes, cacheTokens);
+    }
+
+
+    /**
+     * OK HTTP Interceptor that injects a client credentials Bearer token into a request
+     */
+    @ConditionalOnProperty(prefix = "webservice", name = "jwt")
+    @ConditionalOnMissingBean(name = "jwtInterceptor")
+    @Bean
+    TokenInterceptor jwtInterceptor(@Autowired TokenService tokenService) {
+        return new TokenInterceptor(tokenService);
+    }
+}
