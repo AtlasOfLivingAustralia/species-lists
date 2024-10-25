182f7d3267a1d8a203a672a3de57c6d81a75bdba
Bruce Hyslop <bruce.hyslop@csiro.au>
2024-10-25 14:36:48 +1100

    CICD - trigger deploy after build

--- cicd/build/pipeline/deploy_pipeline.sh
+++ cicd/build/pipeline/deploy_pipeline.sh
@@ -141,6 +141,7 @@ aws cloudformation deploy \
         pBootstrapStackName=$BOOTSTRAP_STACK_NAME \
         pBucketsStackName=$BUCKETS_STACK_NAME \
         pCleanBranch=$clean_branch \
+        pDeployPipelineStack=$DEPLOY_PIPELINE_STACK_NAME \
         pEnvironment=$environment \
         pGitHubBranch=$branch \
         pGitHubOwner=$GITHUB_OWNER \
