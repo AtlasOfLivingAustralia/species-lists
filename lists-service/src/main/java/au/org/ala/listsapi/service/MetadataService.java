2df19f361b71d66492f9a29ed0432b705f5deeb0
Jack Brinkman <jack.brinkman@csiro.au>
2024-10-30 11:22:36 +1000

    Made the setMeta method throw an error if metadata creation fails

--- lists-service/src/main/java/au/org/ala/listsapi/service/MetadataService.java
+++ lists-service/src/main/java/au/org/ala/listsapi/service/MetadataService.java
@@ -35,35 +35,37 @@ public class MetadataService {
     }
 
     public void setMeta(SpeciesList speciesList) {
-        try {
-            String dataResourceUid = speciesList.getDataResourceUid();
-            String entityUid = dataResourceUid != null ? "/" + dataResourceUid : "";
+        String dataResourceUid = speciesList.getDataResourceUid();
+        String entityUid = dataResourceUid != null ? "/" + dataResourceUid : "";
 
-            Map response = webService.post(
-                    collectoryUrl + "/ws/dataResource" + entityUid,
-                    listToDataResourceJSON(speciesList),
-                    null,
-                    ContentType.APPLICATION_JSON,
-                    true,
-                    false,
-                    null
-            );
+        Map response = webService.post(
+                collectoryUrl + "/ws/dataResource" + entityUid,
+                listToDataResourceJSON(speciesList),
+                null,
+                ContentType.APPLICATION_JSON,
+                true,
+                false,
+                null
+        );
 
-            if (speciesList.getDataResourceUid() == null) {
-                // The dataResourceUid is returned via the location header as a URL, i.e.
-                // location = https://collections-test.ala.org.au/ws/dataResource/dr22893
-                Map<String, List<String>> headers = (Map<String, List<String>>) response.get("headers");
-                String location = headers.get("location").get(0);
-
-                if (location != null) {
-                    String[] locationParts = location.split("/");
-                    speciesList.setDataResourceUid(locationParts[locationParts.length - 1]);
-                }
-            }
-
-            logger.info(response.get("resp").toString());
-        } catch (Exception ex) {
-            logger.error("Failed to create metadata entry for '" + speciesList.getTitle() + "' species list", ex);
+        int statusCode = (int)response.get("statusCode");
+        if (statusCode < 200 || statusCode > 299) {
+            logger.error(response.get("error").toString());
+            throw new Error("Failed to create metadata entry for species list");
         }
+
+        if (speciesList.getDataResourceUid() == null) {
+            // The dataResourceUid is returned via the location header as a URL, i.e.
+            // location = https://collections-test.ala.org.au/ws/dataResource/dr22893
+            Map<String, List<String>> headers = (Map<String, List<String>>) response.get("headers");
+            String location = headers.get("location").get(0);
+
+            if (location != null) {
+                String[] locationParts = location.split("/");
+                speciesList.setDataResourceUid(locationParts[locationParts.length - 1]);
+            }
+        }
+
+        logger.info(response.get("resp").toString());
     }
 }
