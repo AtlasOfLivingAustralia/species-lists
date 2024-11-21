2f60ffac55885014bb3dcf5a1c501d24767755ca
Joe Lipson <joe.lipson@csiro.au>
2024-11-21 16:03:58 +1100

    revert CORS *, update app client

--- lists-service/src/main/java/au/org/ala/listsapi/WebConfig.java
+++ lists-service/src/main/java/au/org/ala/listsapi/WebConfig.java
@@ -11,11 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**").
-        allowedOrigins("*").
-        allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").
-        allowedHeaders("*").
-        allowCredentials(true).
-        maxAge(3600);
+    registry.addMapping("/**");
   }
 }
