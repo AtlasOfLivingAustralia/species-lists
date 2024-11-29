cf78317b958c2adb962cbe13325db83b72a953b4
Joe Lipson <joe.lipson@csiro.au>
2024-11-29 11:06:01 +1100

    double quotes for string literals

--- lists-service/src/main/java/au/org/ala/listsapi/WebConfig.java
+++ lists-service/src/main/java/au/org/ala/listsapi/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**").
-        allowedOrigins('https://lists.develop.dev.ala.org.au').
+        allowedOrigins("https://lists.develop.dev.ala.org.au").
         allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").
         allowedHeaders("*").
         allowCredentials(true).
