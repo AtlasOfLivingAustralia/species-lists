b8f0c041b7855ddadd6e1a572ef791e94881de34
Joe Lipson <joe.lipson@csiro.au>
2024-11-29 15:53:53 +1100

    revert container upgrades
    doing this on a separate branch

--- lists-service/src/main/java/au/org/ala/listsapi/WebConfig.java
+++ lists-service/src/main/java/au/org/ala/listsapi/WebConfig.java
@@ -9,16 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-  //@Value("${APP_URL}")
-  //private String appUrl;
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**").
-        allowedOrigins("https://lists-develop.dev.ala.org.au").
-        allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").
-        allowedHeaders("*").
-        allowCredentials(true).
-        maxAge(3600);
+    registry.addMapping("/**");
   }
 }
