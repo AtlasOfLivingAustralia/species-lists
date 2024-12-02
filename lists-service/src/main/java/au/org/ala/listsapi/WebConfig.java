8ad3fbf33d1a553c153294132638630d756db3f5
Joe Lipson <joe.lipson@csiro.au>
2024-12-02 16:27:22 +1100

    include https

--- lists-service/src/main/java/au/org/ala/listsapi/WebConfig.java
+++ lists-service/src/main/java/au/org/ala/listsapi/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**").
-        allowedOrigins(appUrl).
+        allowedOrigins("https://" + appUrl).
         allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").
         allowedHeaders("*").
         allowCredentials(false).
