5a4f59c7d0029a950282aa82fe9d560648333420
Joe Lipson <joe.lipson@csiro.au>
2024-12-02 14:11:25 +1100

    add import

--- lists-service/src/main/java/au/org/ala/listsapi/WebConfig.java
+++ lists-service/src/main/java/au/org/ala/listsapi/WebConfig.java
@@ -1,5 +1,6 @@
 package au.org.ala.listsapi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
