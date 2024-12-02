6913b9349fe80ab24a6c40aead5e1b7efce0f199
Joe Lipson <joe.lipson@csiro.au>
2024-12-02 11:16:45 +1100

    move host config to top level
    simplify config
    rename subdomain vars
    use var in CORS config

--- lists-service/src/main/java/au/org/ala/listsapi/WebConfig.java
+++ lists-service/src/main/java/au/org/ala/listsapi/WebConfig.java
@@ -9,13 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-  //@Value("${APP_URL}")
-  //private String appUrl;
+  @Value("${app.url}")
+  private String appUrl;
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**").
-        allowedOrigins("https://lists-update-os.dev.ala.org.au").
+        allowedOrigins(appUrl).
         allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").
         allowedHeaders("*").
         allowCredentials(true).
