e1e6852c6da9cd60623bd4aa6e881b0ce657d0d7
Jack Brinkman <jack.brinkman@csiro.au>
2025-02-03 15:20:34 +1000

    Tweaked rematching flow, added additional logic

--- lists-service/src/main/java/au/org/ala/listsapi/service/TaxonService.java
+++ lists-service/src/main/java/au/org/ala/listsapi/service/TaxonService.java
@@ -221,39 +221,43 @@ public class TaxonService {
   }
 
   public long taxonMatchDataset(String speciesListID) {
-    logger.info("Taxon matching " + speciesListID);
-
     Optional<SpeciesList> optionalSp = speciesListMongoRepository.findById(speciesListID);
     if (optionalSp.isEmpty()) return 0;
 
     int size = 100;
     int page = 0;
-    long distinct = 0;
-    boolean done = false;
+    Pageable paging = PageRequest.of(page, size);
 
-    while (!done) {
-      Pageable paging = PageRequest.of(page, size);
+    long distinct = 0;
+    boolean finished = false;
+    while (!finished) {
       Page<SpeciesListItem> speciesListItems =
           speciesListItemMongoRepository.findBySpeciesListID(speciesListID, paging);
 
-      if (!speciesListItems.getContent().isEmpty()) {
+      if (speciesListItems.isEmpty()) {
+        finished = true;
+      } else {
+        logger.info("Taxon matching " + speciesListID + " page " + page + " page size " + speciesListItems.stream().count());
         try {
           List<SpeciesListItem> items = speciesListItems.getContent();
+
+          logger.info("Matching for: " + items.stream().map(SpeciesListItem::getScientificName).collect(Collectors.joining(",")));
+
           updateClassifications(items);
+
+          speciesListItemMongoRepository.saveAll(items);
+          progressService.addMongoProgress(speciesListID, items.size());
 
           distinct += items.stream()
                   .filter(speciesListItem -> speciesListItem.getClassification().getSuccess())
                   .map(speciesListItem -> speciesListItem.getClassification().getTaxonConceptID()).distinct().count();
 
-          speciesListItemMongoRepository.saveAll(items);
-          progressService.addMongoProgress(speciesListID, items.size());
+          page += 1;
+          paging = PageRequest.of(page, size);
         } catch (Exception e) {
           logger.error(e.getMessage(), e);
         }
-      } else {
-        done = true;
       }
-      page++;
     }
 
     logger.info("Taxon matching " + speciesListID + " complete.");
