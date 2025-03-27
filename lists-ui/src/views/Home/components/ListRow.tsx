daf060a1ebc1b74c2978b0dfadcd1bd3364702db
Nick dos Remedios <nick.dosremedios@csiro.au>
2025-03-27 15:20:23 +1100

    Make "Public" icon visible on my lists and for admins
    Minor UI fixes

--- lists-ui/src/views/Home/components/ListRow.tsx
+++ lists-ui/src/views/Home/components/ListRow.tsx
@@ -7,13 +7,16 @@ import { SpeciesList } from '#/api';
 import { FolderIcon } from '@atlasoflivingaustralia/ala-mantine';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-regular-svg-icons';
+import { useALA } from '#/helpers/context/useALA';
 
 interface ListRowProps {
   list?: SpeciesList;
+  isUser?: boolean;
 }
 
-export function ListRow({ list }: ListRowProps) {
+export function ListRow({ list, isUser }: ListRowProps) {
   const loading = Boolean(list);
+  const ala = useALA();
   const lastUpdatedObj: Date | undefined = parseDate(list?.lastUpdated || '');
   const lastUpdated: string = lastUpdatedObj instanceof Date && !isNaN(lastUpdatedObj.getTime()) 
       ? formatISO(lastUpdatedObj, { representation: 'date' }) 
@@ -29,10 +32,25 @@ export function ListRow({ list }: ListRowProps) {
             </Anchor>
           </Skeleton>
           <Skeleton w='50%' visible={!loading}>
-            <Group gap='xs'>
+            <Group gap='5'>
+              <Text size='sm' c='dark.5'><FormattedMessage id="date.lastUpdated" defaultMessage="Updated"/>:</Text>
+              <Text size='sm' c='dark.9' fw='500'>{lastUpdated}</Text>
+            </Group>
+          </Skeleton>
+        </Stack>
+      </Table.Td>
+      <Table.Td width={150} style={{ verticalAlign: 'top' }}>
+        <Skeleton visible={!loading}>
+          <Stack gap={4}>
+            <Group gap='4'>
+              <FolderIcon color='grey' />
+              <Text size='sm' c='dark.5'><FormattedNumber value={list?.rowCount || 0} />{' '}</Text>
+              <Text size='sm' c='dark.5'>{ list?.rowCount === 1 ? <FormattedMessage id="taxon" /> : <FormattedMessage id="taxa" /> }</Text>
+            </Group>
+            <Group gap='4'>
               <Badge 
                   variant="outline" 
-                  color="dark.3" 
+                  color="dark.4" 
                   size="md"
                   radius="md"
                   styles={{
@@ -43,28 +61,18 @@ export function ListRow({ list }: ListRowProps) {
                   }}
               >
                 <FormattedMessage id={list?.listType || 'OTHER'} />
-                </Badge>
-              <Text size='sm' c='dark.5'><FormattedMessage id="date.lastUpdated" />: {lastUpdated}</Text>
+              </Badge>
             </Group>
-          </Skeleton>
-        </Stack>
-      </Table.Td>
-      <Table.Td width={125} style={{ verticalAlign: 'top' }}>
-        <Skeleton visible={!loading}>
-          <Stack gap={4}>
-            <Group gap='xs'>
-              <FolderIcon color='grey' />
-              <FormattedNumber value={list?.rowCount || 0} />{' '}
-              { list?.rowCount === 1 ? <FormattedMessage id="taxon" /> : <FormattedMessage id="taxa" /> }
-            </Group>
-            <Group gap='xs'>
-              <FontAwesomeIcon
-                fontSize={14}
-                icon={list?.isPrivate ? faEyeSlash : faEye}
-                color='grey'
-              />
-              {list?.isPrivate ? 'Private' : 'Public'}
-            </Group>
+            {ala.isAuthenticated && (isUser || ala.isAdmin) && (
+              <Group gap='xs'>
+                <FontAwesomeIcon
+                  fontSize={14}
+                  icon={list?.isPrivate ? faEyeSlash : faEye}
+                  color='grey'
+                />
+                {list?.isPrivate ? 'Private' : 'Public'}
+              </Group>
+            )}
           </Stack>
         </Skeleton>
       </Table.Td>
