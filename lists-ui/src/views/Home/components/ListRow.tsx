305b51db056d7cd38523dce99a380ba7b83aa3e6
Nick dos Remedios <nick.dosremedios@csiro.au>
2025-03-28 11:59:14 +1100

    Added attributes + icons to list rows

--- lists-ui/src/views/Home/components/ListRow.tsx
+++ lists-ui/src/views/Home/components/ListRow.tsx
@@ -1,13 +1,15 @@
-import { Anchor, Badge, Group, Skeleton, Stack, Table, Text } from '@mantine/core';
-import { FormattedMessage, FormattedNumber } from 'react-intl';
+import { Anchor, Badge, Group, Skeleton, Space, Stack, Table, Text } from '@mantine/core';
+import { FormattedDate, FormattedMessage, FormattedNumber } from 'react-intl';
 import { Link } from 'react-router';
-import { formatISO } from 'date-fns';
 import { parseDate } from '#/helpers/utils/parseListDate';
 import { SpeciesList } from '#/api';
-import { FolderIcon } from '@atlasoflivingaustralia/ala-mantine';
+import { MapLayersIcon } from '@atlasoflivingaustralia/ala-mantine';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faEyeSlash } from '@fortawesome/free-regular-svg-icons';
+import { faCalendar, faEye, faEyeSlash } from '@fortawesome/free-regular-svg-icons';
 import { useALA } from '#/helpers/context/useALA';
+import { faShieldHalved } from '@fortawesome/free-solid-svg-icons';
+import { faStar } from '@fortawesome/free-regular-svg-icons';
+import { ReactComponent as AlaIcon } from '#/static/ala-logo-grey.svg';
 
 interface ListRowProps {
   list?: SpeciesList;
@@ -17,40 +19,26 @@ interface ListRowProps {
 export function ListRow({ list, isUser }: ListRowProps) {
   const loading = Boolean(list);
   const ala = useALA();
+  
   const lastUpdatedObj: Date | undefined = parseDate(list?.lastUpdated || '');
-  const lastUpdated: string = lastUpdatedObj instanceof Date && !isNaN(lastUpdatedObj.getTime()) 
-      ? formatISO(lastUpdatedObj, { representation: 'date' }) 
-      : 'Unknown';
+  const lastUpdated = lastUpdatedObj instanceof Date && !isNaN(lastUpdatedObj.getTime()) ? (
+    <FormattedDate value={lastUpdatedObj} />
+  ) : 'Unknown';
 
   return (
     <Table.Tr>
       <Table.Td>
-        <Stack gap={4} pb={4}>
+        <Stack gap={4} pb={6} pt={6}>
           <Skeleton visible={!loading}>
             <Anchor size='md' fw={600} component={Link} to={`list/${list?.id}`}>
               {list?.title || 'List title'}
             </Anchor>
           </Skeleton>
-          <Skeleton w='50%' visible={!loading}>
+          <Skeleton w='70%' visible={!loading}>
             <Group gap='5'>
-              <Text size='sm'><FormattedMessage id="date.lastUpdated" defaultMessage="Updated"/>:</Text>
-              <Text size='sm' fw='500'>{lastUpdated}</Text>
-            </Group>
-          </Skeleton>
-        </Stack>
-      </Table.Td>
-      <Table.Td width={150} style={{ verticalAlign: 'top' }}>
-        <Skeleton visible={!loading}>
-          <Stack gap={4}>
-            <Group gap='4'>
-              <FolderIcon color='grey'/>
-              <Text size='sm' ><FormattedNumber value={list?.rowCount || 0} />{' '}</Text>
-              <Text size='sm' >{ list?.rowCount === 1 ? <FormattedMessage id="taxon" /> : <FormattedMessage id="taxa" /> }</Text>
-            </Group>
-            <Group gap='4'>
               <Badge 
                   variant="outline" 
-                  color="gray" 
+                  color="dark.3" 
                   size="md"
                   radius="md"
                   styles={{
@@ -62,16 +50,56 @@ export function ListRow({ list, isUser }: ListRowProps) {
               >
                 <FormattedMessage id={list?.listType || 'OTHER'} />
               </Badge>
+              {ala.isAuthenticated && (isUser || ala.isAdmin) && (
+                <>
+                  <Space w={3} />
+                  <FontAwesomeIcon fontSize={15} color='grey' icon={list?.isPrivate ? faEyeSlash : faEye} />
+                  <Text size='sm' fw='400'>
+                  {list?.isPrivate ? (
+                    <FormattedMessage id="access.private" defaultMessage="Private" />
+                  ) : (
+                    <FormattedMessage id="access.public" defaultMessage="Public" />
+                  )}
+                  </Text>
+                </>
+              )}
+              { list?.isAuthoritative && (
+                <>
+                  <Space w={3} />
+                  <FontAwesomeIcon icon={faStar} fontSize={16} color='grey'/>
+                  <Text size='sm' fw='400'><FormattedMessage id="list.isAuthoritative" defaultMessage="Authoritative"/></Text>
+                </>
+              )}
+              { list?.isSDS && (
+                <>
+                  <Space w={3} />
+                  <FontAwesomeIcon icon={faShieldHalved} fontSize={16} color='grey'/>
+                  <Text size='sm' fw='400'><FormattedMessage id="list.isSDS" defaultMessage="Sensitive"/></Text>
+                </>
+              )}
+              { list?.isBIE && (
+                <>
+                  <Space w={3} />
+                  <AlaIcon size={15} color='grey' width="22px" height="18px"/>
+                  <Text size='sm' fw='400' ml={-1}><FormattedMessage id="list.isBIE" defaultMessage="Species page"/></Text>
+                </>
+              )}
             </Group>
-            {ala.isAuthenticated && (isUser || ala.isAdmin) && (
-              <Group gap='xs'>
-                <FontAwesomeIcon
-                  fontSize={14}
-                  icon={list?.isPrivate ? faEyeSlash : faEye}
-                />
-                {list?.isPrivate ? 'Private' : 'Public'}
-              </Group>
-            )}
+          </Skeleton>
+        </Stack>
+      </Table.Td>
+      <Table.Td width={150} style={{ verticalAlign: 'top' }}>
+        <Skeleton visible={!loading}>
+          <Stack gap={4} pb={6} pt={6}>
+            <Group gap='4'>
+              <MapLayersIcon size={15} color='grey'/>
+              <Text size='sm' fw='400' ml={3}><FormattedNumber value={list?.rowCount || 0} />{' '}</Text>
+              <Text size='sm' fw='400'>{ list?.rowCount === 1 ? <FormattedMessage id="taxon" /> : <FormattedMessage id="taxa" /> }</Text>
+            </Group>
+            <Group gap='4'>
+              <FontAwesomeIcon icon={faCalendar} fontSize={15} color='grey'/>
+              <Text size='sm' fw='400' ml={5}>{lastUpdated}</Text>
+            </Group>
           </Stack>
         </Skeleton>
       </Table.Td>
