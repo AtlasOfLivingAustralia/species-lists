cb33c3d5b878d0f2087959c692f2794dda8d1f1e
Nick dos Remedios <nick.dosremedios@csiro.au>
2025-05-13 14:20:25 +1000

    #289 Help doco added to upload and reingest

--- lists-ui/src/views/Reingest/index.tsx
+++ lists-ui/src/views/Reingest/index.tsx
@@ -1,29 +1,17 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import '@mantine/dropzone/styles.css';
-
-import { ReactNode, useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import {
-  Alert,
-  Badge,
   Button,
-  Flex,
+  Container,
   Group,
   Paper,
   Stack,
   Text,
 } from '@mantine/core';
 import { useDocumentTitle } from '@mantine/hooks';
-import { Dropzone, FileWithPath } from '@mantine/dropzone';
-import {
-  ArrowUpIcon,
-  CautionIcon,
-  FolderIcon,
-  StopIcon,
-  TickIcon,
-} from '@atlasoflivingaustralia/ala-mantine';
 import { notifications } from '@mantine/notifications';
 import { useParams } from 'react-router';
-import { FormattedMessage, FormattedNumber, useIntl } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 // Helpers & local components
 import { performGQLQuery, queries, SpeciesList, UploadResult } from '#/api';
@@ -31,17 +19,17 @@ import { getErrorMessage } from '#/helpers';
 import { IngestProgress } from '#/components/IngestProgress';
 import { useALA } from '#/helpers/context/useALA';
 import { getAccessToken } from '#/helpers/utils/getAccessToken';
-import classes from './index.module.css';
+import { FileUploadDropzone } from '#/components/FileUploadDropzone';
 
-const ACCEPTED_TYPES: string[] = ['text/csv', 'application/zip'];
-
+/**
+ * Reingest component for reingesting species lists
+ * 
+ * @returns {JSX.Element} - The rendered Reingest component
+ */
 export default function Reingest() {
-
   const { id } = useParams();
-  const [uploading, setUploading] = useState<boolean>(false);
   const [ingesting, setIngesting] = useState<boolean>(false);
   const [error, setError] = useState<string | Error | null>(null);
-  const [originalName, setOriginalName] = useState<string | null>(null);
   const [result, setResult] = useState<UploadResult | null>(null);
   const [meta, setMeta] = useState<SpeciesList | null>(null);
   const [_loading, setLoading] = useState(true);
@@ -77,20 +65,9 @@ export default function Reingest() {
     fetchData();
   }, [id]);
 
-  // Callback handler for upload
-  const handleUpload = useCallback(async (files: FileWithPath[]) => {
-    try {
-      setError(null);
-      setUploading(true);
-      setOriginalName(files[0].name);
-
-      const uploadResult = await ala.rest.lists.upload(files);
-      setResult(uploadResult);
-    } catch (error) {
-      setError(getErrorMessage(error as string));
-    }
-
-    setUploading(false);
+  // Handler for successful file upload
+  const handleUploadSuccess = useCallback((uploadResult: UploadResult) => {
+    setResult(uploadResult);
   }, []);
 
   // Callback handler for ingestion
@@ -99,124 +76,30 @@ export default function Reingest() {
     try {
       await ala.rest.lists.reingest(meta?.id || '', result?.localFile || '');
     } catch (error) {
-      // console.error(error);
+      setIngesting(false);
       notifications.show({
         message: getErrorMessage(error),
         position: 'bottom-left',
         radius: 'md',
       });
     }
-  }, [result?.localFile]);
+  }, [result?.localFile, meta?.id]);
 
   const handleReset = useCallback(() => {
     setError(null);
     setResult(null);
   }, []);
 
-  let idle: { title: ReactNode; content: ReactNode; icon: ReactNode } = {
-    title: intl.formatMessage({ id: 'reingest.upload.list.desription', defaultMessage: 'Drag list here, or click to select' }),
-    content: (
-      <>
-        <Text size='sm' c='dimmed' inline>
-          <FormattedMessage id='reingest.upload.list.acceptedFiles' defaultMessage='Accepted files'/>
-        </Text>
-        <Badge>CSV</Badge>
-        <Badge>ZIP</Badge>
-      </>
-    ),
-    icon: <ArrowUpIcon size={40} />,
-  };
-
-  if (error) {
-    idle = {
-      title: intl.formatMessage({ id: 'reingest.error.message.title', defaultMessage: 'An error has occurred' }),
-      content: (
-        <Text size='sm' c='dimmed'>
-          {getErrorMessage(error)}, <FormattedMessage id='reingest.error.message.tryAgain' defaultMessage='please try again with another file'/>
-        </Text>
-      ),
-      icon: <CautionIcon size={40} />,
-    };
-  } else if (result) {
-    idle = {
-      title: `${intl.formatMessage({ id: 'reingest.success.message.prefix', defaultMessage: 'Uploaded' })} ${originalName}`,
-      content: (
-        <>
-          <Badge> {<FormattedNumber value={result.rowCount} />} rows</Badge>
-          <Text lineClamp={2} size='sm' c='dimmed'>
-            <b><FormattedMessage id='reingest.additional.files' defaultMessage='Additional Fields'/>:</b>{' '}
-            {result.fieldList.length > 0 ? result.fieldList.join(', ') : 'N/A'}
-          </Text>
-        </>
-      ),
-      icon: <FolderIcon size={40} />,
-    };
-  }
-
-  const uploadDisabled = Boolean(result);
-
   return (
-    <Stack>
-      <Dropzone
-        onDrop={handleUpload}
-        accept={ACCEPTED_TYPES}
-        radius='lg'
-        loading={uploading}
-        disabled={uploadDisabled}
-        maxSize={52428800}
-        className={uploadDisabled ? classes.disabled : undefined}
-        multiple={false}
-      >
-        <Flex
-          className={classes.inner}
-          justify='center'
-          align='center'
-          gap='lg'
-          style={{ height: result ? 100 : 180 }}
-        >
-          <Dropzone.Accept>
-            <TickIcon size={40} />
-          </Dropzone.Accept>
-          <Dropzone.Reject>
-            <StopIcon size={40} />
-          </Dropzone.Reject>
-          <Dropzone.Idle>{idle.icon}</Dropzone.Idle>
-
-          <div style={{ overflow: 'hidden' }}>
-            <Text size='xl' truncate='end' inline>
-              {idle.title}
-            </Text>
-            <Group gap='xs' align='flex-end' mt={7}>
-              {idle.content}
-            </Group>
-          </div>
-        </Flex>
-      </Dropzone>
-      <div
-        style={{
-          overflow: 'hidden',
-          transition: 'all ease 200ms',
-          height: result?.validationErrors ? 100 : 0,
-        }}
-      >
-        <Alert icon={<CautionIcon />} radius='lg'>
-          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            {(result?.validationErrors || []).map((message) => (
-              <FormattedMessage key={message} id={message} />
-            ))}
-            <Button
-              size='xs'
-              miw={80}
-              ml='sm'
-              variant='outline'
-              color='dark'
-              onClick={() => setResult(null)}
-            >
-              <FormattedMessage id='reingest.upload.tryAgain' defaultMessage='Try again'/>
-            </Button>
-          </div>
-        </Alert>
-      </div>
+    <Container size="lg">
+      <FileUploadDropzone
+        onUploadSuccess={handleUploadSuccess} 
+        onReset={handleReset}
+        uploadType='reingest'
+        showHelpText={true}
+        initialTitle={intl.formatMessage({ id: 'reingest.upload.description', defaultMessage: 'Drag list here, or click to select' })}
+      />
+      
       {result && !result.validationErrors && !ingesting && (
         <Paper p='md' radius='lg' withBorder>
           <Group justify='space-between'>
@@ -238,6 +121,6 @@ export default function Reingest() {
         </Paper>
       )}
       <IngestProgress id={id ?? null} ingesting={ingesting} />
-    </Stack>
+    </Container>
   );
 }
