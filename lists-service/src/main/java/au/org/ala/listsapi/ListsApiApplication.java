515366379c43dbd60840a3ae47d92c5a4b604fa6
Nick dos Remedios <nick.dosremedios@csiro.au>
2025-05-19 14:14:09 +1000

    Removed unused imports

--- lists-service/src/main/java/au/org/ala/listsapi/ListsApiApplication.java
+++ lists-service/src/main/java/au/org/ala/listsapi/ListsApiApplication.java
@@ -10,17 +10,12 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
-
-import java.util.Locale;
 
 @SpringBootApplication
 @EnableAsync
--- lists-service/src/main/java/au/org/ala/listsapi/WebConfig.java
+++ lists-service/src/main/java/au/org/ala/listsapi/WebConfig.java
@@ -3,7 +3,6 @@ package au.org.ala.listsapi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
