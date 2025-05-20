cf5ad1d85b3a00da00b52d1ac47009103a3c6b7c
Nick dos Remedios <nick.dosremedios@csiro.au>
2025-05-20 19:02:59 +1000

    Code review - remove code not needed

--- lists-service/src/main/java/au/org/ala/listsapi/service/ValidationService.java
+++ lists-service/src/main/java/au/org/ala/listsapi/service/ValidationService.java
@@ -10,14 +10,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.ProxySelector;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,17 +27,10 @@ public class ValidationService {
   @Value("${userDetails.api.url}")
   private String userDetailsUrl;
 
-  @Value("${constraints.userdetails.countries.enabled:false}")
-  private boolean userDetailsCountriesEnabled;
-
   private Map<String, List<ConstraintListItem>> constraints = null;
 
   private List<ConstraintListItem> getConstraintsByKey(ConstraintType constraintType) {
     return constraints.get(constraintType.name());
-  }
-
-  private void setConstraintsByKey(ConstraintType constraintType, List<ConstraintListItem> items) {
-    constraints.put(constraintType.name(), items);
   }
 
   @PostConstruct
@@ -60,41 +47,6 @@ public class ValidationService {
       // Handle exceptions appropriately (e.g., logging, re-throwing a custom exception)
       throw new RuntimeException("Failed to read or parse constraints file from: " + constraintsFile.getDescription(), e);
     }
-
-    // TODO: Review whether this should be removed or not. No lists in the legacy lists app
-    // use coutries apart from Australia and New Zealand. So suggesting this be removed.
-    if (userDetailsCountriesEnabled) {
-      try {
-        List<Location> userdetailsCountries = fetchJson(userDetailsUrl + "/ws/registration/countries.json", new TypeReference<>() {});
-        List<ConstraintListItem> countries = getConstraintsByKey(ConstraintType.region);
-
-        // Map the countries list into UI constraints
-        userdetailsCountries.forEach(e -> {
-          var constraint = new ConstraintListItem(e.getIsoCode(), e.getName());
-          countries.add(constraint);
-        });
-
-        setConstraintsByKey(ConstraintType.region, countries);
-      } catch (Exception ex) {
-        log.error("Error loading country constraints from userdetails", ex);
-      }
-    }
-  }
-
-  private <T> T fetchJson(String uri, TypeReference<T> type) throws Exception {
-    HttpRequest httpRequest =
-            HttpRequest.newBuilder(new URI(uri))
-                    .GET()
-                    .build();
-
-    HttpResponse<String> response =
-            HttpClient.newBuilder()
-                    .proxy(ProxySelector.getDefault())
-                    .build()
-                    .send(httpRequest, HttpResponse.BodyHandlers.ofString());
-
-    ObjectMapper objectMapper = new ObjectMapper();
-    return objectMapper.readValue(response.body(), type);
   }
 
   // Returns the constraint list as a map of List<ConstraintListItem> to correctly output JSON
