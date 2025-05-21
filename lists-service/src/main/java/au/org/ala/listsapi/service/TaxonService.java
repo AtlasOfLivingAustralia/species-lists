07d63b931830929ba99053db51b865d952d9bc27
Nick dos Remedios <nick.dosremedios@csiro.au>
2025-05-21 14:17:41 +1000

    Code cleanup

--- lists-service/src/main/java/au/org/ala/listsapi/service/TaxonService.java
+++ lists-service/src/main/java/au/org/ala/listsapi/service/TaxonService.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright (C) 2025 Atlas of Living Australia
+ * All Rights Reserved.
+ *
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ */
 package au.org.ala.listsapi.service;
 
 import au.org.ala.listsapi.controller.MongoUtils;
@@ -12,7 +26,6 @@ import co.elastic.clients.elasticsearch._types.aggregations.Aggregation;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.net.ProxySelector;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
