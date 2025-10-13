e4060d97e970a8db47a3364938bf2b24482b52f7
Nick dos Remedios <nick.dosremedios@csiro.au>
2025-10-13 11:08:12 +1100

    Catch undefined value

--- lists-ui/src/views/Dashboard/index.tsx
+++ lists-ui/src/views/Dashboard/index.tsx
@@ -56,7 +56,7 @@ function Dashboard() {
             auth.signinRedirect();
           }
         }}
-        homeUrl={import.meta.env.VITE_ALA_HOME_PAGE}
+        homeUrl={import.meta.env.VITE_ALA_HOME_PAGE || ''}
         onSearchClick={() => (window.location.href = 'https://bie.ala.org.au')
         }
         fullWidth 
