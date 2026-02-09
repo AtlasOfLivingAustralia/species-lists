d7a119a0e8a55dba11a3cf05f97b02db6e041a20
Nick dos Remedios <nick.dosremedios@csiro.au>
2026-02-09 19:18:03 +1100

    Remove uneeded methods - lombok provided

--- lists-service/src/main/java/au/org/ala/listsapi/model/SpeciesListItem.java
+++ lists-service/src/main/java/au/org/ala/listsapi/model/SpeciesListItem.java
@@ -143,38 +143,4 @@ public class SpeciesListItem {
         return this.classs;
     }
 
-    public ObjectId getId() { return id; }
-    public String getScientificName() { return scientificName; }
-    public List<KeyValue> getProperties() { return properties; }
-    public Classification getClassification() { return classification; }
-    public String getSpeciesListID() { return speciesListID; }
-    public String getTaxonID() { return taxonID; }
-    public String getSuppliedName() { return suppliedName; }
-    public String getVernacularName() { return vernacularName; }
-    public String getKingdom() { return kingdom; }
-    public String getPhylum() { return phylum; }
-    public String getOrder() { return order; }
-    public String getFamily() { return family; }
-    public String getGenus() { return genus; }
-    public Date getDateCreated() { return dateCreated; }
-    public Date getLastUpdated() { return lastUpdated; }
-    public String getLastUpdatedBy() { return lastUpdatedBy; }
-    
-    public void setScientificName(String scientificName) { this.scientificName = scientificName; }
-    public void setTaxonID(String taxonID) { this.taxonID = taxonID; }
-    public void setGenus(String genus) { this.genus = genus; }
-    public void setFamily(String family) { this.family = family; }
-    public void setOrder(String order) { this.order = order; }
-    public void setClasss(String classs) { this.classs = classs; }
-    public void setPhylum(String phylum) { this.phylum = phylum; }
-    public void setKingdom(String kingdom) { this.kingdom = kingdom; }
-    public void setVernacularName(String vernacularName) { this.vernacularName = vernacularName; }
-    public void setProperties(List<KeyValue> properties) { this.properties = properties; }
-    public void setLastUpdated(Date lastUpdated) { this.lastUpdated = lastUpdated; }
-    public void setLastUpdatedBy(String lastUpdatedBy) { this.lastUpdatedBy = lastUpdatedBy; }
-    public void setSpeciesListID(String speciesListID) { this.speciesListID = speciesListID; }
-    public void setClassification(Classification classification) { this.classification = classification; }
-    public void setId(ObjectId id) { this.id = id; }
-    public void setSuppliedName(String suppliedName) { this.suppliedName = suppliedName; }
-    public void setDateCreated(Date dateCreated) { this.dateCreated = dateCreated; }
 }
