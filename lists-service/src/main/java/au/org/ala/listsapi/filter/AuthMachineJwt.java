03a0e46df69f176cfd32f026a427348808940026
Nick dos Remedios <nick.dosremedios@csiro.au>
2026-09-16 09:48:18 +1000

    Revert "Potential fix for pull request finding"
    This reverts commit 69c4dee9f8668849971d4faedbf87fa4612257f7.

--- lists-service/src/main/java/au/org/ala/listsapi/filter/AuthMachineJwt.java
+++ lists-service/src/main/java/au/org/ala/listsapi/filter/AuthMachineJwt.java
@@ -236,7 +236,7 @@ public class AuthMachineJwt extends OncePerRequestFilter {
     }
 
     private Set<String> extractScopes(OidcCredentials credentials) {
-return new HashSet<>(credentials.getAccessToken().getScope().toStringList());
+        Object scope = credentials.getAccessToken().get("scope");
         if (scope instanceof Collection<?> values) {
             return values.stream().map(String::valueOf).collect(java.util.stream.Collectors.toSet());
         }
