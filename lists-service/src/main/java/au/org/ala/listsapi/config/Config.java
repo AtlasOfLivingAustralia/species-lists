e4da686d92e358d7ee8eb0d3b066184dd198d6c3
Nick dos Remedios <nick.dosremedios@csiro.au>
2026-09-16 16:22:43 +1000

    Fix for ES v8 returning 400 error

--- lists-service/src/main/java/au/org/ala/listsapi/config/Config.java
+++ lists-service/src/main/java/au/org/ala/listsapi/config/Config.java
@@ -1,18 +1,22 @@
 package au.org.ala.listsapi.config;
 
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.apache.hc.client5.http.ssl.DefaultClientTlsStrategy;
+import org.apache.hc.client5.http.ssl.HostnameVerificationPolicy;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.core5.http.Header;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.client.elc.rest5_client.Rest5Clients;
 import org.springframework.web.client.RestTemplate;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-import java.security.SecureRandom;
-import java.security.cert.X509Certificate;
 
 @Configuration
 public class Config extends ElasticsearchConfiguration {
@@ -49,8 +53,19 @@ public class Config extends ElasticsearchConfiguration {
                 }
         }, new SecureRandom());
 
-        HostnameVerifier allowAllHosts = (hostname, session) -> true;
-        terminalBuilder = maybeSecureBuilder.usingSsl(sslContext, allowAllHosts);
+        terminalBuilder = maybeSecureBuilder
+            .usingSsl(sslContext, NoopHostnameVerifier.INSTANCE)
+            .withClientConfigurer(
+                Rest5Clients.ElasticsearchConnectionManagerCallback.from(
+                    cmBuilder -> cmBuilder.setTlsStrategy(
+                        new DefaultClientTlsStrategy(
+                            sslContext,
+                            HostnameVerificationPolicy.CLIENT,
+                            NoopHostnameVerifier.INSTANCE
+                        )
+                    )
+                )
+            );
       } catch (Exception e) {
         throw new RuntimeException("Failed to configure SSL context", e);
       }
@@ -61,6 +76,21 @@ public class Config extends ElasticsearchConfiguration {
     if (elasticAuthEnabled) {
       terminalBuilder = terminalBuilder.withBasicAuth(elasticUsername, elasticPassword);
     }
+
+    terminalBuilder = terminalBuilder.withClientConfigurer(
+        Rest5Clients.ElasticsearchHttpClientConfigurationCallback.from(
+            httpClientBuilder -> httpClientBuilder.addRequestInterceptorLast((request, entity, context) -> {
+                Header accept = request.getFirstHeader("Accept");
+                if (accept != null && accept.getValue() != null && accept.getValue().contains("compatible-with=9")) {
+                    request.setHeader("Accept", accept.getValue().replace("compatible-with=9", "compatible-with=8"));
+                }
+                Header contentType = request.getFirstHeader("Content-Type");
+                if (contentType != null && contentType.getValue() != null && contentType.getValue().contains("compatible-with=9")) {
+                    request.setHeader("Content-Type", contentType.getValue().replace("compatible-with=9", "compatible-with=8"));
+                }
+            })
+        )
+    );
 
     return terminalBuilder
             .withSocketTimeout(20000)
